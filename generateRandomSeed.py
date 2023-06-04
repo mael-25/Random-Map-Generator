aec1657cff715ef18f2924c8bbbd1597bebb5c62
Mael <livet.yang@gmail.com>
2023-06-04 12:21:16 +0800

    random seed-generation-modification

--- generateRandomSeed.py
+++ generateRandomSeed.py
@@ -3,6 +3,6 @@ import datetime
 def getSeed(seed):
     if seed == -1: 
         tim = datetime.datetime.now()
-        randseed = tim.hour*3600000+tim.minute*60000+tim.second*1000+tim.microsecond
+        randseed = tim.month*3600000*31*24+tim.day*3600000*24+tim.hour*3600000+tim.minute*60000+tim.second*1000+tim.microsecond
         return randseed
     return seed 
