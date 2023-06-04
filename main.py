e5aa7e7f56c27172d2553319248f3b2d8b49d7e9
Mael <livet.yang@gmail.com>
2023-06-04 17:45:41 +0800

    /beauty/ update

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ screen = pygame.display.set_mode(SCREENSIZE)
 
 
 def draw_grid(screen, grid):
-    pygame.draw.rect(screen, (0,0,0), [MARGIN, MARGIN, DIMENTIONS[1]*SQUAREWIDTH,DIMENTIONS[0]*SQUAREHEIGHT], width=5)
+    pygame.draw.rect(screen, (255,255,255), [MARGIN, MARGIN, DIMENTIONS[1]*SQUAREWIDTH,DIMENTIONS[0]*SQUAREHEIGHT])
     for i, x in enumerate(grid):
         for i2, y in enumerate(x):
             if y == 1: 
@@ -35,7 +35,7 @@ def draw_grid(screen, grid):
 
 running = True
 while running:
-    screen.fill((255,255,255))
+    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
