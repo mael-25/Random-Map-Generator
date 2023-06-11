98f055daf3848e3d7c9c56b31dd9fd8070f72a74
Mael <livet.yang@gmail.com>
2023-06-11 17:31:07 +0800

    need to commit...

--- mapGeneration.py
+++ mapGeneration.py
@@ -149,7 +149,7 @@ def generate_map(seed=-1, seed_of_the_day=False):
     grid = final_grid2
     grid.dtype = np.uint
     
-
+    
     finished = False
     initial_node = Node((0,0), unchecked_directions=get_directions((0,0), grid))
     node_list = [initial_node]
